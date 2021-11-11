93112c315c9be56857295661119c1b05c883ee5d
MacBook <macbook@MacBooks-MacBook-Pro.local>
2021-11-11 15:32:56 +0200

    ChatBot 3-rd stage

--- src/ChatBot.java
+++ src/ChatBot.java
@@ -12,6 +12,13 @@ public class ChatBot {
         Scanner scanner = new Scanner(System.in);
         String your_name = scanner.nextLine();
         System.out.println("What a great name you have, " + your_name + "!");
+        //Part3
+        System.out.println("Let me guess your age.\nEnter remainders of dividing your age by 3, 5 and 7.");
+        int remainder3 = scanner.nextInt();
+        int remainder5 = scanner.nextInt();
+        int remainder7 = scanner.nextInt();
+        int your_age = (remainder3*70+remainder5*21+remainder7*15)%105;
+        System.out.println("Your age is "+ your_age+" ; s a good time to start programming!");
     }
 }
 
