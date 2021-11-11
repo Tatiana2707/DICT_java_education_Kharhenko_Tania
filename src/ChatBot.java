838a331b747cc7409c10ad15287e68c6607f191f
MacBook <macbook@MacBooks-MacBook-Pro.local>
2021-11-11 15:40:31 +0200

    ChatBot 5-th stage

--- src/ChatBot.java
+++ src/ChatBot.java
@@ -19,6 +19,24 @@ public class ChatBot {
         int remainder7 = scanner.nextInt();
         int your_age = (remainder3*70+remainder5*21+remainder7*15)%105;
         System.out.println("Your age is "+ your_age+" ; s a good time to start programming!");
+        //Part4
+        System.out.println("I can count. Enter the number and I will count to it.");
+        int number = scanner.nextInt();
+        for (int a=0; a<number+1; a++) {
+            System.out.println(a + "!");
+            if (a == number) {
+                System.out.println("Готово!");}
+        }
+        //Part5
+        System.out.println("Let's check your knowledge of French. You need to choose the correct answer to my question.\nQuestion: how is the word BOOK translated into French?");
+        System.out.println("\n1.Книга\n2.Carte\n3.Livre\n4.Pencil");
+        while (true) {
+            int user_answer = scanner.nextInt();
+            if(user_answer==3){
+                System.out.println("Well done, "+your_name+"!\n Develop further the French language! Goodbye, have a nice day!");
+                break;}
+            else {
+                System.out.println("Please, try again!");}
+        }
     }
 }
-
