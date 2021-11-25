5cf37a12dffec19e1c2e8025ad5d997ae855e0d8
Tania <macbook@Tania-Kharchenko.local>
2021-11-25 10:49:28 +0200

    Hangman 3 stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import static java.lang.Math.random;
+
 public class Hangman {
     public static void main (String[] arg){
         System.out.println("The game will be available soon");
@@ -7,6 +9,23 @@ public class Hangman {
         String correct_word;
         correct_word = "java";
         Scanner in = new Scanner(System.in);
+        int min;
+        int max;
+        min=1;
+        max=4;
+        int cloose_word = min +(int)(random()*(max-min+1));
+        if (cloose_word==1){
+            correct_word = "python";
+        }
+        else if (cloose_word == 2){
+            correct_word = "java";
+        }
+        else if (cloose_word == 3){
+            correct_word = "javascript";
+        }
+        else if (cloose_word == 4){
+            correct_word = "kotlin";
+        }
         word = in.nextLine();
 
         if (correct_word.equals(word)){
