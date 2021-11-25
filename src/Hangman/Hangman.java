9a935184c345ef62dc21f17a6300cf84b6d2044c
Tania <macbook@Tania-Kharchenko.local>
2021-11-25 10:32:44 +0200

    Hangman 2 stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,2 +1,19 @@
-package Hangman;public class Hangman {
+import java.util.Scanner;
+
+public class Hangman {
+    public static void main (String[] arg){
+        System.out.println("The game will be available soon");
+        String word;
+        String correct_word;
+        correct_word = "java";
+        Scanner in = new Scanner(System.in);
+        word = in.nextLine();
+
+        if (correct_word.equals(word)){
+            System.out.println("YOU SURVIED");
+        }
+        else {
+            System.out.println("YOU LOST");
+        }
+    }
 }
