b619d7d83f2602b4d3f6553f3421eb78113d66c1
Tania <macbook@Tania-Kharchenko.local>
2021-12-07 15:17:31 +0200

    CoffeMachine 2 stage

--- src/CoffeMachine/CoffeMachine.java
+++ src/CoffeMachine/CoffeMachine.java
@@ -1,11 +1,21 @@
+import java.util.Scanner;
+
 public class CoffeMachine {
     public static void main (String[]args){
-        System.out.println("Starting to make a coffe\n " +
-                "Grinding coffe beans\n" +
-                "Boiling water\n" +
-                "Mixing boild water with crushed coffe beans\n"+
-                "Pouring coffe into the cup\n"+
-                "Pouring some milk into the cup\n "+
-                "Coffe is realy!\n");
+        Scanner in = new Scanner(System.in);
+        int cups;
+        int water;
+        int milk;
+        int coffe_beans;
+        System.out.println("Write how many cups of coffe you will need: ") ;
+                cups= in.nextInt();
+                water=cups*200;
+                milk=cups*50;
+                coffe_beans=cups*15;
+
+                System.out.println("For"+cups+"cups of coffe you will need");
+                System.out.println(water + "ml of Water");
+                System.out.println(milk + "ml of Milk");
+                System.out.println(coffe_beans + "g of coffe beans");
     }
 }
